5777a5b1e302a331087b2a4fc648065061809125
Mika Jokinen <jokisen.mika@gmail.com>
2021-01-19 12:00:08 +0000

    Muokattiin lokifilen sijainti

--- configure-vm01.sh
+++ configure-vm01.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -o /var/tmp/localscript.sh https://raw.githubusercontent.com/MikaJokinen/2021Public/master/configure-vm01-local.sh
 chmod 777 /var/tmp/localscript.sh
-/var/tmp/localscript.sh &> /var/log/localscript.log
+/var/tmp/localscript.sh &> /var/tmp/localscript.log
