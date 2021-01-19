934fae68aaeae26862db87959417a6e67b71cb8c
Mika Jokinen <jokisen.mika@gmail.com>
2021-01-19 10:52:21 +0000

    date

--- configure-vm01.sh
+++ configure-vm01.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -O - https://raw.githubusercontent.com/MicrosoftDocs/mslearn-ansible-control-machine/master/configure-ansible-centos.sh | sudo bash
 sudo yum install -y nmap
-
+wget -O - https://raw.githubusercontent.com/MicrosoftDocs/mslearn-ansible-control-machine/master/configure-ansible-centos.sh | sudo bash
